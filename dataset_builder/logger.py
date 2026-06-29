17b122b07267f477fcd96ad0a1d5b8769c732217
apexajay-rc <ajaykrishnanh.dev@gmail.com>
2026-06-29 13:51:14 +0530

    Update logger.py

--- dataset_builder/logger.py
+++ dataset_builder/logger.py
@@ -1,1 +1,21 @@
+"""
+Logging configuration
+"""
 
+import logging
+
+from config import LOG_DIR, LOG_FILE, LOG_LEVEL
+
+LOG_DIR.mkdir(parents=True, exist_ok=True)
+
+logging.basicConfig(
+
+    filename=LOG_FILE,
+
+    level=LOG_LEVEL,
+
+    format="%(asctime)s | %(levelname)s | %(message)s"
+
+)
+
+logger = logging.getLogger("dataset_builder")
