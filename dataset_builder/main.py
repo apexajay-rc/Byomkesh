43713c7e4035718aaa5c4bf25c0193e718ca1871
apexajay-rc <ajaykrishnanh.dev@gmail.com>
2026-06-29 13:51:26 +0530

    Update main.py

--- dataset_builder/main.py
+++ dataset_builder/main.py
@@ -1,1 +1,40 @@
+"""
+Dataset Builder Main Loop
+"""
 
+import time
+
+from collector import TelemetryCollector
+from writer import DatasetWriter
+from metadata import MetadataManager
+from logger import logger
+
+from config import SCRAPE_INTERVAL
+
+
+collector = TelemetryCollector()
+
+writer = DatasetWriter()
+
+metadata = MetadataManager()
+
+metadata.create()
+
+logger.info("Dataset Builder started.")
+
+
+while True:
+
+    try:
+
+        record = collector.collect()
+
+        writer.write(record)
+
+        logger.info("Collected telemetry snapshot.")
+
+    except Exception as e:
+
+        logger.exception(e)
+
+    time.sleep(SCRAPE_INTERVAL)
